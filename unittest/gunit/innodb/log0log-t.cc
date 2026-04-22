a767ec52f157643edc7a7baa02d61d4983b2be6c
Yasufumi Kinoshita <buildup.db@gmail.com>
2026-04-23 01:44:02 +0900

    Couldn't build unit_test for not-debug build
    Following fix for RP018.
    unittest/gunit/innodb/log0log-t.cc is only for UNIV_DEBUG now.

--- unittest/gunit/innodb/log0log-t.cc
+++ unittest/gunit/innodb/log0log-t.cc
@@ -20,6 +20,7 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
+#ifdef UNIV_DEBUG
 
 #include "univ.i"
 
@@ -748,3 +749,4 @@ TEST(log0log, log_users) { test_single("log_users"); }
 TEST(log0log, log_closer) { test_single("log_closer"); }
 
 TEST(log0log, log_checkpointer) { test_single("log_checkpointer"); }
+#endif /* UNIV_DEBUG */
