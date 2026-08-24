b9650084ced5d223bc9e2423b130aea712b3671b
Yasufumi Kinoshita <buildup.db@gmail.com>
2026-08-24 14:37:02 +0900

    [RP034] Reordered buf_page_t members order with cacheline boundary conscious
    The buf_page_t struct in 8.0 has two differences from 5.7 that affect performance scale.
    
    (1) sizeof(buf_page_t) is 136, but was 128 (just cacheline x 2) at 5.7.
    
      The rest part of buf_block_t members might be arranged with sizeof(buf_page_t)==128.
      (alignment with -8 from cacheline boundary seems better scale.)
    
      The new and not-so-frequent used member has been moved to the head of buf_page_t,
      and -8 cacheline boundary allocation for the buf_block_t array in the buffer pool.
    
    (2) buf_page_t::buf_fix_count became std::atomic and used very frequently.
    
      The other "Very frequent read" or "Frequent updated" members should not be located
      at the same cacheline with the buf_page_t::buf_fix_count
    
      Rearranged the order of buf_page_t members.

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -1079,7 +1079,8 @@ static buf_chunk_t *buf_chunk_init(
   /* Allocate the block descriptors from
   the start of the memory block. */
   chunk->blocks =
-      (buf_block_t *)ut_align(chunk->mem, ut::INNODB_CACHE_LINE_SIZE);
+      (buf_block_t *)((ulint)ut_align(chunk->mem, ut::INNODB_CACHE_LINE_SIZE) +
+                      ut::INNODB_CACHE_LINE_SIZE - sizeof(uint64_t));
 
   /* Align a pointer to the first frame.  Note that when
   os_large_page_size is smaller than UNIV_PAGE_SIZE,
